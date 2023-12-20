83ca93005b6bdd0c767b08f3652fd60f79ca3792
TVlearns <t.akselvinje@gmail.com>
2023-12-19 16:08:19 -0800

    Updated static directory

--- backend/financebackend/settings.py
+++ backend/financebackend/settings.py
@@ -147,10 +147,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = "/usr/src/app/static"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/dist'),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
